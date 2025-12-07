f9559042955098c78fc1bd0a1114ef210d55439f
jwno <161667580+jwnome@users.noreply.github.com>
2025-12-07 16:13:36 -0600

    Fix command typo in shutdown script

--- .config/niri/shutdown.sh
+++ .config/niri/shutdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 killall waybar
-swww img ~/Wallpapers/wallpaper-black.png --transition-type simple --transition-duration 0.25
+awww img ~/Wallpapers/wallpaper-black.png --transition-type simple --transition-duration 0.25
 sleep 0.3
 systemctl poweroff
